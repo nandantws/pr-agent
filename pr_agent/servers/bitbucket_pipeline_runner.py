030c9d01829baa2caad9ddb0b4edf7c9c225d561
sarbjitgrewal <sarbjitgrewaltekkiweb@gmail.com>
2023-09-08 14:02:06 +0530

    add pipeline runner

--- pr_agent/servers/bitbucket_pipeline_runner.py
+++ pr_agent/servers/bitbucket_pipeline_runner.py
@@ -4,9 +4,8 @@ from pr_agent.tools.pr_reviewer import PRReviewer
 
 for key, value in os.environ.items():
     print(f"{key}: {value}==================")
-    if "BITBUCKET_PR_ID" in key:
-        print('PR ID found')
-        pr_url = f"https://bitbucket.org/tejinder22/blog/pull-requests/11"
-        print(f"PR URL: {pr_url}")
-        PRReviewer(pr_url).run()
+    print('PR ID found')
+    pr_url = f"https://bitbucket.org/tejinder22/blog/pull-requests/11"
+    print(f"PR URL: {pr_url}")
+    PRReviewer(pr_url).run()
 
