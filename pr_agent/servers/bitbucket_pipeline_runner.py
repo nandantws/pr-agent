e39b7a0009995b7903b21aa09b609233a06c33c6
sarbjitgrewal <sarbjitgrewaltekkiweb@gmail.com>
2023-09-08 15:36:32 +0530

    update docker-compose

--- pr_agent/servers/bitbucket_pipeline_runner.py
+++ pr_agent/servers/bitbucket_pipeline_runner.py
@@ -1,12 +1,14 @@
 import os
 from pr_agent.agent.pr_agent import PRAgent
 from pr_agent.tools.pr_reviewer import PRReviewer
+import asyncio
 
-for key, value in os.environ.items():
-    print(f"{key}: {value}==================")
-    print('PR ID found')
-    pr_url = f"https://bitbucket.org/tejinder22/blog/pull-requests/11"
-    if  "BITBUCKET_PR_ID" in key:
-        print(f"PR URL: {pr_url}")
-        PRReviewer(pr_url).run()
+async def run_action():
+    for key, value in os.environ.items():
+        print(f"{key}: {value}==================")
+        print('PR ID found')
+        pr_url = f"https://bitbucket.org/tejinder22/blog/pull-requests/11"
+        if  "BITBUCKET_PR_ID" in key:
+            print(f"PR URL: {pr_url}")
+            await PRReviewer(pr_url).run()
 
