115f110ecb57be840c908b736092b8aaac60e41c
sarbjitgrewal <sarbjitgrewaltekkiweb@gmail.com>
2023-09-13 11:19:35 +0530

    update config file

--- your_script.py
+++ your_script.py
@@ -1,11 +1,11 @@
 print('Hello worldww')
 
 import os
-bitbucket_bearer_token = os.environ.get("BITBUCKET_BEARER_TOKEN")
+bitbucket_bearer_token = os.environ.get("OPENAI_API_KEY")
 print(f"bearer: {bitbucket_bearer_token}===============================")
 
-for key, value in os.environ.items():
-    print('===============')
-    print(f"{key}: {value}")
-    if "BITBUCKET_PR_ID" in key:
-        print('PR ID found')
+# for key, value in os.environ.items():
+#     print('===============')
+#     print(f"{key}: {value}")
+#     if "BITBUCKET_PR_ID" in key:
+#         print('PR ID found')
