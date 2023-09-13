3f8f8eb57192bde77637341a335fdb2a5ab9af3a
sarbjitgrewal <sarbjitgrewaltekkiweb@gmail.com>
2023-09-13 15:46:54 +0530

    update config file

--- pr_agent/git_providers/bitbucket_provider.py
+++ pr_agent/git_providers/bitbucket_provider.py
@@ -13,12 +13,13 @@ from .git_provider import FilePatchInfo, GitProvider
 
 class BitbucketProvider(GitProvider):
     def __init__(
-        self, pr_url: Optional[str] = None, incremental: Optional[bool] = False, oauth_token=None
+        self, pr_url: Optional[str] = None, incremental: Optional[bool] = False, env_vars:Optional[str]=None
     ):
+        self.env_vars = env_vars
         s = requests.Session()
         try:
-            if oauth_token != None:
-                s.headers["Authorization"] = f"Bearer {oauth_token}"
+            if self.env_vars != None:
+                s.headers["Authorization"] = f"Bearer {self.env_vars[0]}"
             else:
                 bearer = context.get("bitbucket_bearer_token", None)
                 s.headers["Authorization"] = f"Bearer {bearer}"
