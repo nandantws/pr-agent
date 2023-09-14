f0845eb0f5d1fab4e3fbfe9a093eb2646646699f
sarbjitgrewal <sarbjitgrewaltekkiweb@gmail.com>
2023-09-14 17:31:03 +0530

    update pipeline

--- pr_agent/servers/bitbucket_pipeline_runner.py
+++ pr_agent/servers/bitbucket_pipeline_runner.py
@@ -1,5 +1,6 @@
 import os
 from pr_agent.agent.pr_agent import PRAgent
+from pr_agent.config_loader import get_settings
 from pr_agent.tools.pr_reviewer import PRReviewer
 import asyncio
 
@@ -9,6 +10,15 @@ async def run_action():
     workspace = os.environ.get("BITBUCKET_WORKSPACE", '')
     bearer_token = os.environ.get('BITBUCKET_BEARER_TOKEN', None)
     print(bearer_token, '=-=-=-=-=-=-=-=-=-=-=')
+
+    # Check if required environment variables are set
+    if not bearer_token:
+        print("BITBUCKET_BEARER_TOKEN not set")
+
+    # Set the environment variables in the settings
+    get_settings().set("BITBUCKET.BEARER_TOKEN", bearer_token)
+    print(get_settings, '=-=-=-=-=-=-=--=-=-ff')
+
     if pull_request_id and slug and workspace:
         pr_url = f"https://bitbucket.org/{workspace}/{slug}/pull-requests/{pull_request_id}"
         await PRReviewer(pr_url, env_vars=[bearer_token]).run()
